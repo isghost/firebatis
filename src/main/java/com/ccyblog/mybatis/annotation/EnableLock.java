4afab2378a614c8b0125b7f41f52b30fb3ec1a59
BG315420 <cychen@best-inc.com>
2018-03-20 15:12:47 +0800

    初始化工程

---
+++ src/main/java/com/ccyblog/mybatis/annotation/EnableLock.java
@@ -0,0 +1,20 @@
+package com.ccyblog.mybatis.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author BG315420
+ * @date 2018/3/19 11:22
+ * @desc sql自动添加version字段
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface EnableLock {
+    boolean value() default false ;
+    boolean enableException() default false;
+}
